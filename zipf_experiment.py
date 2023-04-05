e9799fb37fd555f9bc917afa3292e8d96337b31a
richieb <richieb@gmail.com>
2023-04-04 21:11:57 -0400

    Fixed to work with Python 3.

--- zipf_experiment.py
+++ zipf_experiment.py
@@ -10,11 +10,11 @@ import Pmf
 frequency_table = {}
 
 def load_file (fname):
-    print "Loading words from file: %s " % fname
+    print ("Loading words from file: %s " % fname)
     wg = word_reader.word_getter(fname)
     for w in wg:
         key = w.lower()
-        if (frequency_table.has_key(key)):
+        if key in frequency_table:
             frequency_table[key] += 1
         else:
             frequency_table[key] = 1
@@ -23,11 +23,11 @@ def load_file (fname):
 if __name__ == "__main__":
     load_file ("moby_dick.txt")
     load_file ("war_and_peace.txt")
-    print "Word table size: %d"  % len(frequency_table)
+    print ("Word table size: %d"  % len(frequency_table))
 
     pmf = Pmf.MakeHistFromList(frequency_table.values())
     cdf = Cdf.MakeCdfFromPmf(pmf)
 
-    print "Percentile 95 = %d" % (cdf.Percentile(95))
-    print "Mean = %f " % (cdf.Mean())
+    print ("Percentile 95 = %d" % (cdf.Percentile(95)))
+    print ("Mean = %f " % (cdf.Mean()))
 
